13f6c4898b5f19d5805fddf3f9f85c523ea427a7
Ben Heavner <bheavner@gmail.com>
2022-07-10 06:23:45 -0700

    passed lexicon tests, starting number parsing

--- ex_49/lexicon.py
+++ ex_49/lexicon.py
@@ -1,11 +1,28 @@
 game_lexicon = {
-    'direction': ['north', 'south', 'east', 'west', 'down', 'up', 'left', 'right', 'back']
+    'directions': ['north', 'south', 'east', 'west', 'down', 'up', 'left', 'right', 'back'],
+    'verbs': ['go', 'stop', 'kill', 'eat'],
+    'stops': ['the', 'in', 'of', 'from', 'at', 'it'],
+    'nouns': ['door', 'bear', 'princess', 'cabinet']
     }
+
+# a utility function
+def convert_number(s):
+    try:
+        return int(s)
+    except ValueError:
+        return None
 
 def scan(input_string):
     words = input_string.split()
     sentence = []
     for word in words:
-        if word in game_lexicon['direction']:
+        if word in game_lexicon['directions']:
             sentence.append(('direction', word))
+        elif word in game_lexicon['verbs']:
+            sentence.append(('verb', word))
+        elif word in game_lexicon['stops']:
+            sentence.append(('stop', word))
+        elif word in game_lexicon['nouns']:
+            sentence.append(('noun', word))
+
     return(sentence)
--- tests/test_ex_49.py
+++ tests/test_ex_49.py
@@ -2,17 +2,38 @@ from unittest import TestCase
 #from ex_49.lexicon import scan
 from ex_49 import lexicon
 
-class TestBasicFunction(TestCase):
-    def test(self):
+class TestTests(TestCase):
+    def test_tests(self):
         self.assertTrue(True)
 
 class Testex_49(TestCase):
     def test_directions(self):
-        self.assertEqual(lexicon.scan("north"), [('direction', 'north')])
-        result = lexicon.scan("north south east")
+        self.assertEqual(lexicon.scan('north'), [('direction', 'north')])
+
+        result = lexicon.scan('north south east')
         self.assertEqual(result, [('direction', 'north'),
                                   ('direction', 'south'),
                                   ('direction', 'east')])
+
+    def test_verbs(self):
+        self.assertEqual(lexicon.scan('go'), [('verb', 'go')])
+
+        result = lexicon.scan('go kill eat')
+        self.assertEqual(result, [('verb', 'go'),
+                                  ('verb', 'kill'),
+                                  ('verb', 'eat')])
+
+    def test_stops(self):
+        self.assertEqual(lexicon.scan('the'), [('stop', 'the')])
+
+        result = lexicon.scan('the in of')
+        self.assertEqual(result, [('stop', 'the'),
+                                  ('stop', 'in'),
+                                  ('stop', 'of')])
+
+    def test_nounts(self):
+        self.assertEqual(lexicon.scan('bear'), [('noun', 'bear')])
+
 
 if __name__ == '__main__':
     unittest.main()
